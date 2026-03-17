1b65605c61eb6dd9bff8c2b20a7e3b498f1b87c4
sspaciamis21-bit <sspacia.mis21@gmail.com>
2026-03-17 13:21:04 +0530

    removed .mp4 files

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,13 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+   output: 'standalone', // ✅ Critical for VPS deployment
+  poweredByHeader: false,
+  compress: true,
+  env: {
+    APPS_SCRIPT_URL:`https://script.google.com/macros/s/AKfycbwybDS3UOAwsqfHwhbCJLTl0dsTD_sq-u5OwYdIcb_unw5lrWHxtf97P9zc03RKWB5V5Q/exec`,
+    APPS_SCRIPT_TOKEN:`6869ff576b8b0e210637b6a57afd39449c0abbc730fc07d09c626598d77b597c`,
+  }
 };
 
 export default nextConfig;
